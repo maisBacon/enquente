e9e1b6dcbd1bafeedb3f8617e0ef5ee3419681ec
renan <renann.eiras@gmail.com>
2020-10-10 19:11:19 -0300

    chore: ajust jest config

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,11 @@
 module.exports = {
   roots: ['<rootDir>/src'],
   collectCoverageFrom: [
-    '<rootDir>/src/**/*.ts'
-    // '!<rootDir>/src/main/**',
-    // '!<rootDir>/src/**/*-protocols.ts',
-    // '!**/protocols/**',
-    // '!**/test/**'
+    '<rootDir>/src/**/*.ts',
+    '!<rootDir>/src/main/**',
+    '!<rootDir>/src/**/*-protocols.ts',
+    '!**/protocols/**',
+    '!**/test/**'
   ],
   coverageDirectory: 'coverage',
   testEnvironment: 'node',
