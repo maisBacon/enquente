c348aced63042b76ece45272081c16c95de74764
renan <renann.eiras@gmail.com>
2020-10-11 11:59:30 -0300

    refactor: tratando erro com helper 400 e loop de requiredFields

--- src/presentation/controller/signup.ts
+++ src/presentation/controller/signup.ts
@@ -1,17 +1,12 @@
 import { HttpRequest, HttpResponse } from '../protocols/http'
 import { MissingParamsError } from '../error/missing-param-error'
+import { badRequest } from '../helper/http-helper'
 export class SignUpController {
   handle (httpRequest: HttpRequest): HttpResponse {
-    if (!httpRequest.body.name) {
-      return {
-        statusCode: 400,
-        body: new MissingParamsError('name')
-      }
-    }
-    if (!httpRequest.body.email) {
-      return {
-        statusCode: 400,
-        body: new MissingParamsError('email')
+    const requiredFields = ['name', 'email']
+    for (const field of requiredFields) {
+      if (!httpRequest.body[field]) {
+        return badRequest(new MissingParamsError(field))
       }
     }
   }
