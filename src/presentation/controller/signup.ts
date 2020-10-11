3fc0cbb57f8a72e8302d01f12552634836e0f33f
renan <renann.eiras@gmail.com>
2020-10-11 12:13:50 -0300

    refactor: add protocolo no controller SignUpController

--- src/presentation/controller/signup.ts
+++ src/presentation/controller/signup.ts
@@ -1,7 +1,9 @@
+import { Controller } from '../protocols/controller'
 import { HttpRequest, HttpResponse } from '../protocols/http'
 import { MissingParamsError } from '../error/missing-param-error'
 import { badRequest } from '../helper/http-helper'
-export class SignUpController {
+
+export class SignUpController implements Controller {
   handle (httpRequest: HttpRequest): HttpResponse {
     const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
     for (const field of requiredFields) {
